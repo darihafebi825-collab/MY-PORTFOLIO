200ac59f98cb6327a7feee758f88a49169238c59
darihafebi825-collab <darihafebi825@gmail.com>
2026-02-01 20:58:52 +0530

    Initial commit - React project

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./index.css";   // 👈 THIS LINE
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/MY-PORTFOLIO">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
